07f33cbc2c622403e8fa9c8c9af3deb73640aabc
frber <78807896+frber@users.noreply.github.com>
2021-03-30 21:47:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import openpyxl
 
 
 
-def get_check(finansiarer, finansiarer_get):
+def get_check(numnamn_get, projektnummer):
     #print("hej")
-    for x, y in zip(finansiarer_get, finansiarer):
+    for x, y in zip(numnamn_get, projektnummer):
         print(x.get(), y)
        # plats = x[0]
         #print(plats.get())
@@ -21,7 +21,31 @@ def pop_droplist(root, finansiarer):
     drop.pack()
 
 
-def hamta_projekt()
+def hamta_projekt():
+    wb = openpyxl.load_workbook('Docs/Proj.xlsx', data_only=True)
+    ws = wb["Proj"]
+    projnummer = []
+    projnamn = []
+    numnamn = []
+    for row in ws['A1:A100']:
+        for cell in row:
+            konto = cell.value
+            #print(konto)
+            proj = cell.offset(column = 2).value
+            #print(proj)
+            projt = cell.offset(column = 3).value
+            belopp = cell.offset(column = 7).value
+            if konto == None and proj != None and projt != None and belopp != None:
+                print(type(proj))
+                projnummer.append(proj)
+                projnamn.append(projt)
+                numnamn.append(str(proj)+ " "+projt)
+
+    return projnummer, projnamn, numnamn
+
+
+
+
 
 def hamta_fin():
     df = pd.read_excel(r'Docs\Data.xlsx')
@@ -32,22 +56,56 @@ def main():
     root = Tk()
     root.geometry("680x350")
     finansiarer = hamta_fin()
-    projekt = hamta_projekt()
+    projektnummer = hamta_projekt()[0]
+    projektnamn = hamta_projekt()[1]
+    numnamn = hamta_projekt()[2]
+    #print(numnamn)
     finansiarer_get = list(enumerate(finansiarer))
     #pop_droplist(root, finansiarer)
 
-    print(finansiarer)
-    for x in finansiarer_get:
+    numnamn_get = list(enumerate(numnamn))
+
+
+    rad = 0
+    for x in numnamn_get:
         plats = x[0]
-        fin = x[1]
-        finansiarer_get[plats] = IntVar()
-        l = Checkbutton(root, text=fin, variable=finansiarer_get[plats])
-        l.pack()
+        proj = x[1]
+        numnamn_get[plats] = IntVar()
+        l = Checkbutton(root, text=proj, variable=numnamn_get[plats])
+        l.grid(row=rad, column=0, sticky="W")
+
+        #numnamn_get[plats] = StringVar()
+        #numnamn_get[plats].set("Välj finansiär")
+       # drop = OptionMenu(root, numnamn_get[plats], *finansiarer)
+        #drop.grid(row=rad, column=1, sticky="W")
+
+        rad += 1
+
+    B = Button(root, text="get_check", command=lambda: get_check(numnamn_get, projektnummer))
+    B.grid(row=rad + 1, column=0)
 
 
 
-    B = Button(root, text="get_check", command=lambda: get_check(finansiarer, finansiarer_get))
-    B.pack()
+
+
+
+    #rad = 0
+    #for x in finansiarer_get:
+        #plats = x[0]
+        #fin = x[1]
+        #finansiarer_get[plats] = IntVar()
+        #l = Checkbutton(root, text=fin, variable=finansiarer_get[plats])
+        #l.grid(row=rad, column=0)
+        #drop = OptionMenu(root, finansiarer_get[plats], *finansiarer_get)
+        #drop.grid(row=rad, column=1)
+        #rad += 1
+
+
+
+
+
+    #B = Button(root, text="get_check", command=lambda: get_check(finansiarer, finansiarer_get))
+    #B.grid(row=rad+1, column=0)
 
 
 
@@ -57,3 +115,12 @@ def main():
 if __name__ == "__main__":
     main()
 
+#Ge dynamiskt periodiseringsförslag efter val av fin och fingrad?
+#Sätta in förslag i ny eller tidgare använd berper (beroende på namn etc).
+#Skapa rätt berpers med rätt namn utifrån kontering i berper
+#Flytta över till bokföringsmall med/utan rev från berpers
+#Kontrollera fel i berpers
+
+
+#Lägga in data - ex kontrollera vht och göra färdigt en bokföringsorder med rättningsförslag. Kontrollera andra saker
+#Visualisera kontering av personal
