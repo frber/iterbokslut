1eecc169994ed8becf5a68fb2920eab5d13a712a
Fredrik Bergström <fredrik.bergstrom@ltu.se>
2021-03-30 16:38:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from tkinter import *
 import tkinter as tk
 import tkinter.ttk as ttk
 import pandas as pd
+import openpyxl
 
+
+
+def get_check(finansiarer, finansiarer_get):
+    #print("hej")
+    for x, y in zip(finansiarer_get, finansiarer):
+        print(x.get(), y)
+       # plats = x[0]
+        #print(plats.get())
 
 
 def pop_droplist(root, finansiarer):
@@ -10,6 +19,9 @@ def pop_droplist(root, finansiarer):
     clicked.set(finansiarer[0])
     drop = OptionMenu(root, clicked, *finansiarer)
     drop.pack()
+
+
+def hamta_projekt()
 
 def hamta_fin():
     df = pd.read_excel(r'Docs\Data.xlsx')
@@ -20,8 +32,28 @@ def main():
     root = Tk()
     root.geometry("680x350")
     finansiarer = hamta_fin()
-    pop_droplist(root, finansiarer)
+    projekt = hamta_projekt()
+    finansiarer_get = list(enumerate(finansiarer))
+    #pop_droplist(root, finansiarer)
+
+    print(finansiarer)
+    for x in finansiarer_get:
+        plats = x[0]
+        fin = x[1]
+        finansiarer_get[plats] = IntVar()
+        l = Checkbutton(root, text=fin, variable=finansiarer_get[plats])
+        l.pack()
+
+
+
+    B = Button(root, text="get_check", command=lambda: get_check(finansiarer, finansiarer_get))
+    B.pack()
+
+
+
+
     root.mainloop()
 
 if __name__ == "__main__":
     main()
+
