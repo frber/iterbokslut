baa5a853c24a3841a8eab19927c956cb5e155e88
frber <78807896+frber@users.noreply.github.com>
2021-04-01 14:17:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,28 @@ import openpyxl
 
 
 
-def get_check(numnamn_get, projektnummer):
-    #print("hej")
-    for x, y in zip(numnamn_get, projektnummer):
-        print(x.get(), y)
-       # plats = x[0]
-        #print(plats.get())
+def hamta_varden(lista_box, lista_drop):
+    for box, drop in zip(lista_box, lista_drop):
+        print(box.get(), drop.get())
 
 
-def pop_droplist(root, finansiarer):
-    clicked = StringVar()
-    clicked.set(finansiarer[0])
-    drop = OptionMenu(root, clicked, *finansiarer)
-    drop.pack()
+def skapa_box_drop(root, numnamn, finansiarer):
+    lista_box = []
+    lista_drop = []
+    rad = 0
+    for x in numnamn:
+        box = IntVar()
+        drop = StringVar()
+        checkbox = Checkbutton(root, text=x, variable=box)
+        checkbox.grid(row=rad, column=0, sticky="W")
+        droplist = OptionMenu(root, drop, *finansiarer)
+        droplist.grid(row=rad, column=1, sticky="W")
+        lista_box.append(box)
+        lista_drop.append(drop)
+        rad += 1
 
+    knapp = Button(root, text="get_check", command=lambda: hamta_varden(lista_box, lista_drop))
+    knapp.grid(row=rad + 1, column=0)
 
 def hamta_projekt():
     wb = openpyxl.load_workbook('Docs/Proj.xlsx', data_only=True)
@@ -36,16 +44,11 @@ def hamta_projekt():
             projt = cell.offset(column = 3).value
             belopp = cell.offset(column = 7).value
             if konto == None and proj != None and projt != None and belopp != None:
-                print(type(proj))
                 projnummer.append(proj)
                 projnamn.append(projt)
                 numnamn.append(str(proj)+ " "+projt)
 
     return projnummer, projnamn, numnamn
-
-
-
-
 
 def hamta_fin():
     df = pd.read_excel(r'Docs\Data.xlsx')
@@ -55,60 +58,13 @@ def hamta_fin():
 def main():
     root = Tk()
     root.geometry("680x350")
+
     finansiarer = hamta_fin()
     projektnummer = hamta_projekt()[0]
     projektnamn = hamta_projekt()[1]
     numnamn = hamta_projekt()[2]
-    #print(numnamn)
-    finansiarer_get = list(enumerate(finansiarer))
-    #pop_droplist(root, finansiarer)
 
-    numnamn_get = list(enumerate(numnamn))
-
-
-    rad = 0
-    for x in numnamn_get:
-        plats = x[0]
-        proj = x[1]
-        numnamn_get[plats] = IntVar()
-        l = Checkbutton(root, text=proj, variable=numnamn_get[plats])
-        l.grid(row=rad, column=0, sticky="W")
-
-        #numnamn_get[plats] = StringVar()
-        #numnamn_get[plats].set("Välj finansiär")
-       # drop = OptionMenu(root, numnamn_get[plats], *finansiarer)
-        #drop.grid(row=rad, column=1, sticky="W")
-
-        rad += 1
-
-    B = Button(root, text="get_check", command=lambda: get_check(numnamn_get, projektnummer))
-    B.grid(row=rad + 1, column=0)
-
-
-
-
-
-
-    #rad = 0
-    #for x in finansiarer_get:
-        #plats = x[0]
-        #fin = x[1]
-        #finansiarer_get[plats] = IntVar()
-        #l = Checkbutton(root, text=fin, variable=finansiarer_get[plats])
-        #l.grid(row=rad, column=0)
-        #drop = OptionMenu(root, finansiarer_get[plats], *finansiarer_get)
-        #drop.grid(row=rad, column=1)
-        #rad += 1
-
-
-
-
-
-    #B = Button(root, text="get_check", command=lambda: get_check(finansiarer, finansiarer_get))
-    #B.grid(row=rad+1, column=0)
-
-
-
+    skapa_box_drop(root, numnamn, finansiarer)
 
     root.mainloop()
 
