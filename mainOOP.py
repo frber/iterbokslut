83d6551d4e8a7b979caf0fc66a62361f0e7add09
frber <78807896+frber@users.noreply.github.com>
2021-04-16 21:16:32 +0200

    Update mainOOP.py

--- mainOOP.py
+++ mainOOP.py
@@ -14,16 +14,18 @@ class Gui:
     def __init__(self, master):
         self.master = master
         master.title("iterb")
-        master.geometry("600x600")
+        master.geometry("1200x800")
 
         # Skapa tabs
         self.tabcontrol = Notebook(master)
         self.tab1 = Frame(self.tabcontrol)
         self.tabcontrol.add(self.tab1, text="1. Start")
         self.tab2 = Frame(self.tabcontrol)
-        self.tabcontrol.add(self.tab2, text="2. Lägg till/Ta bort projekt")
+        self.tabcontrol.add(self.tab2, text="2. Lägg till/Ta bort Finansiär")
         self.tab3 = Frame(self.tabcontrol)
-        self.tabcontrol.add(self.tab3, text="3. Skapa periodiseringsförslag")
+        self.tabcontrol.add(self.tab3, text="3. Lägg till/Ta bort Projekt")
+        self.tab4 = Frame(self.tabcontrol)
+        self.tabcontrol.add(self.tab4, text="3. Skapa periodiseringsförslag")
         self.tabcontrol.pack(expand=1, fill="both")
 
         # Knapp Leta gamla berpers i - tab1
@@ -51,50 +53,70 @@ class Gui:
         self.knapp_agressodata = Button(self.tab1, text="Lägg in agressodata", command=self.doc_agressodata)
         self.knapp_agressodata.grid(row=3, column=0)
 
-        # Label Projektnummer -tab2
-        self.projnum = Entry(self.tab2, width=30)
+        # Label Projektnummer -tab3
+        self.projnum = Entry(self.tab3, width=30)
         self.projnum.grid(row=1, column=1)
-        self.projnum_label = Label(self.tab2, text="Projektnummer:").grid(row=1, column=0)
+        self.projnum_label = Label(self.tab3, text="Projektnummer:").grid(row=1, column=0)
 
-        # Label Projektnamn -tab2
-        self.projnamn = Entry(self.tab2, width=30)
+        # Label Projektnamn -tab3
+        self.projnamn = Entry(self.tab3, width=30)
         self.projnamn.grid(row=2, column=1)
-        self.projnamn_label = Label(self.tab2, text="Projektnamn:")
+        self.projnamn_label = Label(self.tab3, text="Projektnamn:")
         self.projnamn_label.grid(row=2, column=0)
 
-        # Label Finansieringsgrad -tab2
-        self.fingrad = Entry(self.tab2, width=30)
+        # Label Finansieringsgrad -tab3
+        self.fingrad = Entry(self.tab3, width=30)
         self.fingrad.grid(row=4, column=1)
-        self.fingrad_label = Label(self.tab2, text="Finansieringsgrad:")
+        self.fingrad_label = Label(self.tab3, text="Finansieringsgrad:")
         self.fingrad_label.grid(row=4, column=0)
 
-        # Droplist finansiärer -tab2
+        # Droplist finansiärer -tab3
         self.finansiarer = self.hamta_fin()
         self.fin = StringVar()
-        self.drop = OptionMenu(self.tab2, self.fin, "", *self.finansiarer)
+        self.drop = OptionMenu(self.tab3, self.fin, "", *self.finansiarer)
         self.drop.grid(row=3, column=1, sticky="W")
-        self.fin_label = Label(self.tab2, text="Finansiär:")
+        self.fin_label = Label(self.tab3, text="Finansiär:")
         self.fin_label.grid(row=3, column=0)
 
-        # Droplist projekt från db -tab2
-        self.lista_projekt_i_db = self.hamta_projekt()
-        self.proj_db = StringVar()
-        self.drop2 = OptionMenu(self.tab2, self.proj_db, "", *self.lista_projekt_i_db)
-        self.drop2.grid(row=6, column=1, sticky="W")
+        # Droplist projekt från db -tab3
+        #self.lista_projekt_i_db = self.hamta_projekt()
+        #self.proj_db = StringVar()
+        #self.drop2 = OptionMenu(self.tab3, self.proj_db, "", *self.lista_projekt_i_db)
+        #self.drop2.grid(row=6, column=1, sticky="W")
 
-        # Knapp Lägg till -tab2
-        self.knapp_lagg_till = Button(self.tab2, text="Lägg till", command=self.spara_till_db)
+        # Knapp Lägg till -tab3
+        self.knapp_lagg_till = Button(self.tab3, text="Lägg till", command=self.spara_till_db)
         self.knapp_lagg_till.grid(row=5, column=1)
 
-        # Knapp Ta bort -tab2
-        self.knapp_ta_bort = Button(self.tab2, text="Ta bort", command=self.ta_bort_fran_db)
-        self.knapp_ta_bort.grid(row=7, column=1)
+        # Knapp Ta bort -tab3
+        self.knapp_ta_bort = Button(self.tab3, text="Ta bort", command=self.ta_bort_fran_db)
+        self.knapp_ta_bort.grid(row=8, column=1)
 
+
+        # Träd -tab3
+        self.tree = Treeview(self.tab3)
+        self.tree['columns'] = ("Projektnummer", "Projektnamn", "Finansiär", "% Finansering")
+        self.tree.column("#0", width=0, stretch=NO)
+        self.tree.column("Projektnummer", anchor=W)
+        self.tree.column("Projektnamn", anchor=W)
+        self.tree.column("Finansiär", anchor=W)
+        self.tree.column("% Finansering", anchor=W)
+
+        self.tree.heading("#0", text="", anchor=W)
+        self.tree.heading("Projektnummer", text="Projektnummer", anchor=W)
+        self.tree.heading("Projektnamn", text="Projektnamn", anchor=W)
+        self.tree.heading("Finansiär", text="Finansiär", anchor=W)
+        self.tree.heading("% Finansering", text="% Finansering", anchor=W)
+
+        self.tree.grid(row=7, column=1)
+        self.initiera_trad()
+
+
+        # Prognar -tab4
         #self.s = ttk.Style()
         #self.s.theme_use("winnative")
         #self.s.configure("blue.Horizontal.TProgressbar", foreground='navy', background='navy')
-        self.prog_bar = Progressbar(self.tab3, style="blue.Horizontal.TProgressbar", orient=HORIZONTAL, length=100,
-                                        maximum=100, mode='determinate')
+        self.prog_bar = Progressbar(self.tab4, orient=HORIZONTAL, length=100, maximum=100, mode='indeterminate')
         self.prog_bar.grid(row=1, column=2)
 
 
@@ -102,13 +124,8 @@ class Gui:
         self.uppdatera_boxlista()
 
 
-        # Knapp Ladda in projekt -tab3
-        #self.knapp_ladda_projekt = Button(self.tab3, text="Ladda in projekt", command=self.skapa_perforslag)
-        #self.knapp_ladda_projekt.grid(row=7, column=1)
-
-
-        # Knapp Skapa förslag -tab3
-        self.knapp_skapa_forslag = Button(self.tab3, text="Skapa periodiseringsförslag", command=self.thread)
+        # Knapp Skapa perförslag -tab4
+        self.knapp_skapa_forslag = Button(self.tab4, text="Skapa Periodiseringsförslag", command=self.thread)
         self.knapp_skapa_forslag.grid(row=8, column=4)
 
     def valj_gamla_berpers(self):
@@ -173,10 +190,12 @@ class Gui:
         ws.cell(row=ws.max_row, column=4).value = finansieringsgrad
         wb.save('Docs/Projekt.xlsx')
         wb.close()
-        self.uppdatera_droplist_projekt()
+       #self.uppdatera_droplist_projekt()
         self.uppdatera_boxlista()
+        self.uppdatera_trad_projekt()
 
     def uppdatera_droplist_projekt(self):
+        # Används ej just nu
         self.lista_projekt_i_db = self.hamta_projekt()
         self.drop2.set_menu("", *self.lista_projekt_i_db)
         self.proj_db.set("")
@@ -184,17 +203,78 @@ class Gui:
     def ta_bort_fran_db(self):
         wb = openpyxl.load_workbook('Docs/Projekt.xlsx', data_only=True)
         ws = wb["Projekt"]
-        projnr = self.proj_db.get().split()[0]
+
+        # Hämta värden projektnummer från val i Treeview
+        i = self.tree.focus()
+        d = self.tree.item(i)
+        val = d['values']
+        projnr = val[0]
+
+
         c = 1
         for row in ws['A2:A1000']:
             for cell in row:
                 c+=1
-                if cell.value == projnr:
+                if str(cell.value) == str(projnr):
                     ws.delete_rows(c)
         wb.save('Docs/Projekt.xlsx')
         wb.close()
-        self.uppdatera_droplist_projekt()
+
+        #self.uppdatera_droplist_projekt()
         self.ta_bort_boxar()
+        self.uppdatera_trad_projekt()
+
+    def initiera_trad(self):
+        wb = openpyxl.load_workbook('Docs\\Projekt.xlsx', data_only=True)
+        ws = wb['Projekt']
+        self.lista_trad = []
+        for row in ws['A2:A1000']:
+            for cell in row:
+                projnr =cell.value
+                projnamn = cell.offset(column=1).value
+                fin = cell.offset(column=2).value
+                fingrad = cell.offset(column=3).value
+                if projnr != None:
+                    self.lista_trad.append([projnr, projnamn, fin, fingrad])
+
+        wb.close()
+
+        c = 0
+        for x in self.lista_trad:
+            self.tree.insert(parent='', index='end', iid=c, values=(x[0], x[1], x[2], x[3]))
+            c+=1
+
+    def uppdatera_trad_projekt(self):
+
+        # Cleara Treeview
+        c = 0
+        for x in self.lista_trad:
+            self.tree.delete(c)
+            c+=1
+
+        # Populera ny lista från databas
+        wb = openpyxl.load_workbook('Docs\\Projekt.xlsx', data_only=True)
+        ws = wb['Projekt']
+        self.lista_trad = []
+        for row in ws['A2:A1000']:
+            for cell in row:
+                projnr = cell.value
+                projnamn = cell.offset(column=1).value
+                fin = cell.offset(column=2).value
+                fingrad = cell.offset(column=3).value
+                if projnr != None:
+                    self.lista_trad.append([projnr, projnamn, fin, fingrad])
+
+        wb.close()
+
+        # Sätt in ny lista i Treeview
+        c2 = 0
+        for x in self.lista_trad:
+            self.tree.insert(parent='', index='end', iid=c2, values=(x[0], x[1], x[2], x[3]))
+            c2 += 1
+
+
+
 
     def uppdatera_boxlista(self):
         wb = openpyxl.load_workbook('Docs/Projekt.xlsx', data_only=True)
@@ -217,7 +297,7 @@ class Gui:
             rad3 = 1
             for x in lista_proj:
                 box = IntVar()
-                checkbox = Checkbutton(self.tab3, text=x, variable=box)
+                checkbox = Checkbutton(self.tab4, text=x, variable=box)
                 checkbox.grid(row=rad, column=0, sticky="W")
                 rad += 1
                 if rad > 20:
@@ -244,9 +324,9 @@ class Gui:
         t.start()
 
     def skapa_perforslag(self):
-        self.prog_bar.start(5)
-        filvag_gamla_berpers = r'C:\Users\berfre\Desktop\gamla berper'
-        filvag_spara_berpers = r'C:\Users\berfre\Desktop\testspara'
+        self.prog_bar.start(4)
+        filvag_gamla_berpers = r'C:\Users\Fredrik\Desktop\Gamla berper'
+        filvag_spara_berpers = r'C:\Users\Fredrik\Desktop\Testspara'
         # Lägg till för dynamiskt sen
         #filvag_gamla_berpers = self.filvag_gamla_berpers.get()
         #filvag_spara_berpers = self.filvag_spara_berpers.get()
@@ -267,7 +347,7 @@ class Gui:
         self.prog_bar.stop()
 
 def main():
-    root = ThemedTk(theme="radiance")
+    root = ThemedTk(theme="black")
     gui = Gui(root)
     root.mainloop()
 
