a80ff3a8f663d30adbd6780dca8df1425cf5f8e6
frber <78807896+frber@users.noreply.github.com>
2021-04-20 15:45:48 +0200

    Update mainOOP.py

--- mainOOP.py
+++ mainOOP.py
@@ -9,6 +9,7 @@ import threading
 
 # Egna klasser
 from tab1 import *
+from tab2 import *
 from tab3 import *
 
 class Gui:
@@ -17,19 +18,19 @@ class Gui:
         master.title("iterb")
         master.geometry("1200x800")
         # Skapa tabs
-        self.tabcontrol = Notebook(master)
-        tab1 = Frame(self.tabcontrol)
-        self.tabcontrol.add(tab1, text="1. Start")
-        self.tab2 = Frame(self.tabcontrol)
-        self.tabcontrol.add(self.tab2, text="2. Lägg till/Ta bort Finansiär")
-        tab3 = Frame(self.tabcontrol)
-        self.tabcontrol.add(tab3, text="3. Lägg till/Ta bort Projekt")
-        self.tab4 = Frame(self.tabcontrol)
-        self.tabcontrol.add(self.tab4, text="3. Skapa periodiseringsförslag")
-        self.tabcontrol.pack(expand=1, fill="both")
+        tabcontrol = Notebook(master)
+        tab1 = Frame(tabcontrol)
+        tabcontrol.add(tab1, text="1. Start")
+        tab2 = Frame(tabcontrol)
+        tabcontrol.add(tab2, text="2. Lägg till/Ta bort Finansiär")
+        tab3 = Frame(tabcontrol)
+        tabcontrol.add(tab3, text="3. Lägg till/Ta bort Projekt")
+        #self.tab4 = Frame(self.tabcontrol)
+        #self.tabcontrol.add(self.tab4, text="3. Skapa periodiseringsförslag")
+        tabcontrol.pack(expand=1, fill="both")
 
         tab1 = Tab1(tab1)
-
+        tab2 = Tab2(tab2)
         tab3 = Tab3(tab3, tab1)
 
 
